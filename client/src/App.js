3862f27ae9cb52102a3d95250e790521585cbfe0
partapparam <paramsingh1961@gmail.com>
2022-12-05 22:03:00 -0800

    react setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./Eval_blue.svg"
+import "./App.css"
+import React from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Eval
+      </header>
+    </div>
+  )
+}
+
+export default App
